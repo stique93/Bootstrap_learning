8b9f510dfa814871c1b5cd2a08c60eb3f79f6d86
Dmitriy <44037165+stique93@users.noreply.github.com>
2019-02-18 14:12:09 +0300

    Adding new files to the site structure
    Changing the structure of the site. Add media files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.dropdown-toggle').click(function (e) {
+    if ($(document).width() > 768) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        if (url !== '#') {
+            window.location.href = url;
+        }
+    }
+});
